4e57cdb504cad013e14f6c5d865fbc18e02dccfa
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-09-12 00:47:40 -0400

    Finished Array Exercises

--- src/Arrays/Array.java
+++ src/Arrays/Array.java
@@ -1,5 +1,7 @@
 package Arrays;
 
+import java.util.Arrays;
+
 public class Array {
     //private container to store numbers.
     private int[] items;
@@ -9,6 +11,14 @@ public class Array {
     public Array(int length){
         //initializing the back store container by the size given by user.
         items = new int[length];
+    }
+
+    public int getActualSize(){
+        return items.length;
+    }
+
+    public int size(){
+        return count;
     }
 
 
@@ -68,8 +78,93 @@ public class Array {
         }
         //Will iterate count number of times because count will keep track of Array's
         //true number of items, So if Array is empty nothing will print.
+        int[] compressedArr = new int[count];
         for(int i = 0; i < count; i++){
-            System.out.println(items[i]);
+//            System.out.println(items[i]);
+            compressedArr[i] = items[i];
         }
+        System.out.println(Arrays.toString(compressedArr));
+
     }
+
+
+    /**
+     * Exercise 1:
+     *     Extend the Array class and add a new method to return the largest number.
+     *     What is the runtime complexity of this method?
+     */
+    public int max(){
+        int max = items[0];
+        for (int i = 0; i < count; i++){
+            int comp = Math.max(items[i],max);
+            if(comp>max){
+                max = comp;
+            }
+        }
+        return max;
+    }
+
+
+    /**
+     * Exercise 2:
+     *     Extend the Array class and add a method to return the common items in this array
+     *     and another array.
+     */
+    public Array intersect(Array arr){
+        if(arr.count == 0 || count == 0)
+            throw new IllegalArgumentException();
+        Array retArr = new Array(count + arr.count);
+        for(int i = 0; i < count; i++){
+            for(int j = 0; j < arr.count; j++){
+                if(items[i] == arr.items[j]){
+                    retArr.insert(items[i]);
+                }
+            }
+        }
+        return retArr;
+    }
+
+
+    /**
+     * Exercise 3:
+     *     Extend the Array class and add a method to reverse the array.
+     *     For example, if the array includes [1, 2, 3, 4], after reversing and printing it,
+     *     we should see [4, 3, 2, 1].
+     */
+    public void reverse(){
+        int[] revArr = new int[count];
+        for(int i = count-1; i >= 0; i--){
+            revArr[count-1-i] = items[i];
+        }
+        items = revArr;
+    }
+
+
+    /**
+     * Exercise 4:
+     *     Extend the Array class and add a new method to insert an item at a given index:
+     *     public void insertAt(int item, int index)
+     */
+    public void insertAt(int item, int index){
+        //Check if back store array is full, if so resize Array to twice the prev. size.
+        if(items.length == count){
+            int[] doubleSizedItemsContainer = new int[count*2];
+            //Copy all existing items into the new back store array.
+            for(int i = 0; i < count; i++){
+                doubleSizedItemsContainer[i] = items[i];
+            }
+            //assign double sized array to back store array.
+            items = doubleSizedItemsContainer;
+        }
+        //Access the back store array at the given index and move every item in the array
+        // over to the left by one space up starting from that given index + 1.
+        for(int i = count - 1; i >= index; i--){
+            items[i + 1] = items[i];
+        }
+
+        items[index] = item;
+        //Decrement count so when calling print() we only print the actual values we have.
+        ++count;
+    }
+
 }
--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         numbers.insert(5);
         numbers.insert(10);
+        numbers.insert(10000);
         numbers.insert(15);
         numbers.insert(20);
         numbers.insert(25);
@@ -23,12 +24,52 @@ public class Main {
 
         System.out.println("----------------");
 
-        numbers.indexOf(2);
-
-        System.out.println("----------------");
-
         System.out.println(numbers.indexOf(10));
         System.out.println(numbers.indexOf(20));
 
+        System.out.println("----------------");
+
+        System.out.println(numbers.max());
+
+        System.out.println("----------------");
+
+        Array numbers2 = new Array(5);
+        numbers.print();
+
+        numbers2.insert(5);
+        numbers2.insert(0);
+        numbers2.insert(10000);
+        numbers2.insert(1);
+        numbers2.insert(20);
+        numbers2.insert(2);
+        numbers2.insert(235);
+        numbers2.print();
+
+        System.out.println("----------------");
+
+        numbers.intersect(numbers2).print();
+
+        System.out.println("----------------");
+
+        System.out.println("numbers: ");
+        numbers.print();
+        numbers.reverse();
+        System.out.println("numbers reversed: ");
+        numbers.print();
+
+        System.out.println("----------------");
+
+        numbers.print();
+
+        numbers.insertAt(3, 5);
+        numbers.print();
+
+        numbers.insertAt(1,0);
+        numbers.print();
+
+        numbers.insertAt(7,3);
+        System.out.println("numbers' back store size: " + numbers.getActualSize());
+        System.out.println("numbers' size: " + numbers.size());
+        numbers.print();
     }
 }
