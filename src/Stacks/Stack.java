411ed93d13f7d2062e3c88e4e77f0e3f6504d470
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-03 19:52:34 -0400

    Built Stack via int Array w/ push, pop, peek, isEmpty,& toString methods.

---
+++ src/Stacks/Stack.java
@@ -0,0 +1,36 @@
+package Stacks;
+
+import java.util.Arrays;
+
+public class Stack {
+    public class Stack2{
+        private int[] items = new int[5];
+        private int count = 0;
+
+        public void push(int k){
+            items[count++] = k;
+        }
+
+        public int pop(){
+            if(count == 0){
+                throw new IllegalStateException();
+            }
+            return items[--count];
+        }
+
+        public int peek(){
+            return items[count - 1];
+        }
+
+        public boolean isEmpty(){
+            return count == 0;
+        }
+
+        @Override
+        public String toString(){
+            var content = Arrays.copyOfRange(items, 0, count);
+            return Arrays.toString(content);
+        }
+
+    }
+}
