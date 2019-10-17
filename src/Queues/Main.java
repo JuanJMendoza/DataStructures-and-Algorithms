9f464ebcdc90b1445914ae1914652181dd5d8334
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-17 15:54:27 -0400

    Implemented a reverse function for a queue type object.

--- src/Queues/Main.java
+++ src/Queues/Main.java
@@ -1,7 +1,34 @@
 package Queues;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args){
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println(queue);
+        reverse(queue);
+        System.out.println(queue);
+    }
+
+    public static void reverse(Queue<Integer> queue){
+        // Can only use add, remove, isEmpty to make this function.
+        if (!queue.isEmpty()){
+            Stack<Integer> stack = new Stack<>();
+
+            while (!queue.isEmpty()){
+                stack.push(queue.remove());
+            }
+
+            while (!stack.isEmpty()){
+                queue.add(stack.pop());
+            }
+        }
+
 
     }
 }
