4dde239961224e9ecfb177472c16513a4f54077c
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-17 20:00:26 -0400

    Implemented de/enqueue using two stacks for a custom queue object.

---
+++ src/Queues/StackQueue.java
@@ -0,0 +1,41 @@
+package Queues;
+
+import java.util.Stack;
+
+public class StackQueue {
+    //enqueue, dequeue, peek, isEmpty, isFull
+    private Stack holder = new Stack();
+    private Stack reversingMechOutput = new Stack();
+    private int front;
+
+
+    public void enqueue(int val){
+        holder.push(val);
+    }
+
+
+    public int dequeue(){
+        // if the queue is empty then there is nothing to dequeue and we throw an exception.
+        if(holder.isEmpty()){
+            throw new IllegalStateException();
+        }
+
+        // reverses element dequeueing ordering of holder so we can pop element at the front.
+        while(!holder.isEmpty()){
+            reversingMechOutput.push(holder.pop());
+        }
+
+        // stores front element inside of the variable front.
+        front = (int) reversingMechOutput.pop();
+
+        // reverses the ordering back into holder so when we enqueue it gets added to the back and keeps proper
+        // ordering when we dequeue again.
+        while(!reversingMechOutput.isEmpty()){
+            holder.push(reversingMechOutput.pop());
+        }
+
+        return front;
+    }
+
+
+}
