2fb5b515c5b61ae6e862273c6ce55b313ed673b6
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-17 20:47:09 -0400

    Refractored code.

--- src/Queues/StackQueue.java
+++ src/Queues/StackQueue.java
@@ -5,8 +5,8 @@ import java.util.Stack;
 
 public class StackQueue {
     //enqueue, dequeue, peek, isEmpty, isFull
-    private Stack holder = new Stack();
-    private Stack reversingMechOutput = new Stack();
+    private Stack<Integer> holder = new Stack();
+    private Stack<Integer> reversingMechOutput = new Stack();
 
 
     public void enqueue(int val){
@@ -15,50 +15,35 @@ public class StackQueue {
 
 
     public int dequeue(){
-        int front;
-
         // if the queue is empty then there is nothing to dequeue and we throw an exception.
         if(holder.isEmpty()){
             throw new IllegalStateException();
         }
 
-        // reverses element dequeueing ordering of holder so we can pop element at the front.
-        while(!holder.isEmpty()){
-            reversingMechOutput.push(holder.pop());
-        }
+        TransferElementsFromStack1ToStack2();
 
-        // stores front element inside of the variable front.
-        front = (int) reversingMechOutput.pop();
-
-        // reverses the ordering back into holder so when we enqueue it gets added to the back and keeps proper
-        // ordering when we dequeue again.
-        while(!reversingMechOutput.isEmpty()){
-            holder.push(reversingMechOutput.pop());
-        }
-
-        return front;
+        return reversingMechOutput.pop();
     }
 
     // shows the top element in the queue.
     public int peek(){
-        int peek;
-        // if the queue is empty then there is nothing to peek and we throw an exception.
+        // if the queue is empty then there is nothing to dequeue and we throw an exception.
         if(holder.isEmpty()){
             throw new IllegalStateException();
         }
 
-        while(!holder.isEmpty()){
-            reversingMechOutput.push(holder.pop());
-        }
+        TransferElementsFromStack1ToStack2();
 
-        peek = (int) reversingMechOutput.peek();
-
-        while(!reversingMechOutput.isEmpty()){
-            holder.push(reversingMechOutput.pop());
-        }
-        return peek;
+        return reversingMechOutput.peek();
     }
 
-
+    private void TransferElementsFromStack1ToStack2() {
+        if (reversingMechOutput.isEmpty()) {
+            // reverses element dequeueing ordering of holder so we can pop element at the front.
+            while (!holder.isEmpty()) {
+                reversingMechOutput.push(holder.pop());
+            }
+        }
+    }
 
 }
