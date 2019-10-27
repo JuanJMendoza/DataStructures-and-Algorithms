f1edcca188d1a97a562a1bd12183b9752e431510
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-27 11:51:04 -0400

    Tested firstNonRepeatingChar() in main.

---
+++ src/HashTables/Main.java
@@ -0,0 +1,16 @@
+package HashTables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args){
+        CharFinder finder = new CharFinder();
+        String randS = "A Green Apple";
+
+        System.out.println(finder.firstNonRepeatingCharacter(randS));
+
+    }
+
+
+}
