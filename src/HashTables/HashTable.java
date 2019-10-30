0e5df5463fc20d76ba541a439b621c2ee9d61048
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-10-30 11:26:49 -0400

    Implemented put, & hash functions for hashtable class

--- src/HashTables/HashTable.java
+++ src/HashTables/HashTable.java
@@ -25,6 +25,41 @@ public class HashTable {
 
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
+    public void put(int key, String value){
+        // Hashes the key and outputs the index to which we're going to insert out element
+        // in our array.
+        var index = hash(key);
 
+        // If there is nothing at this particular index, initialize that place in the array
+        // with a linked list.
+        if(entries[index] == null){
+            entries[index] = new LinkedList<>();
+        }
+
+        // Bucket holds the linked list at the particular index of our storing array.
+        var bucket = entries[index];
+
+        // We scan the bucket to see if there exists an entry with the same key[continued in if statement]
+        for (var entry : bucket){
+            // [continued exp.] If so we get that entry with the matching key and replace its
+            // current value with the new incoming one.
+            if(entry.key == key){
+                entry.value = value;
+                // ends entire method.
+                return;
+            }
+        }
+
+        // If we get to this point, this means we didn't find an entry with the same key in our bucket.
+        // We then create a new Entry object for the given key and value and add it to the end of the
+        // Linked list at the particular index of out storing array (bucket).
+        var entry = new Entry(key, value);
+        bucket.addLast(entry);
+    }
+
+    // Hash function to convert key into a valid index based on the size of our array store.
+    private int hash(int key) {
+        return key % entries.length;
+    }
 
 }
