417ac703c4819e36facd8fb93abcd6bda81daf67
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2019-12-31 02:46:13 -0500

    Tested Tree class via Main and debugger.

--- src/Trees/Main.java
+++ src/Trees/Main.java
@@ -1,4 +1,16 @@
 package Trees;
 
 public class Main {
+    public static void main(String[] args){
+        Tree tree = new Tree();
+        tree.insert(13);
+        tree.insert(3);
+        tree.insert(21);
+        tree.insert(1);
+        tree.insert(5);
+        System.out.println("Finished!");
+
+
+    }
+
 }
