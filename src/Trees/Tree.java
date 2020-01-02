bb5432662c0892a627c8f9dc2f0a884620ac2578
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-01-02 18:43:16 -0500

    Implemented traversePreOrder() to traverse tree in PreOrder traversal.

--- src/Trees/Tree.java
+++ src/Trees/Tree.java
@@ -34,7 +34,6 @@ public class Tree {
     private Node root;
 
     /**
-     *
      * @param value is the value we are trying to insert into the Tree.
      */
     public void insert(int value){
@@ -67,7 +66,7 @@ public class Tree {
             }
             // otherwise, the value is larger then the current node's value.
             else if(value > current.value){
-                // we check if the right subtree is empty, if so we know to insert a new node w/ the input value as its value.
+                // if the right subtree is empty, if so we know to insert a new node w/ the input value as its value.
                 if(current.rightChild == null) {
                     // we insert a new node and make it the current node's right child w/ the input value as its value.
                     current.rightChild = new Node(value);
@@ -82,7 +81,6 @@ public class Tree {
     }
 
     /**
-     *
      * @param value parameter is the value we're searching for in out Tree.
      * @return returns a boolean which will convey whether the value is found in the Tree or not.
      */
@@ -92,12 +90,12 @@ public class Tree {
         // until we've found the node with our corresponding value, or we traversed down to a null child,
         // we will continue to traverse down the tree.
         while (current != null){
-            // if the value we're looking for is less than the current node's value we check if it's left child is valid
+            // if the value we're looking for is less than the current node's value, check if its left child is valid
             if(value < current.value){
                 // if there is a left child we make current equal to current node's left child.
                 current = current.leftChild;
             }
-            // if the value we're looking for is greater than the current node's value we check if it's right child is valid
+            // if the value we're looking for is greater than the current node's value, check if its right child is valid
             else if (value > current.value){
                 // if there is a right child we make current equal to current node's right child.
                 current = current.rightChild;
@@ -109,5 +107,30 @@ public class Tree {
         }
         // we exited the loop b/c we traversed past a leaf to a null child, or the root was null, and return false.
         return false;
+    }
+
+    /**
+     * Recursive method to traverse tree in preOrder traversal(root, left, right).
+     */
+    public void traversePreOrder(){
+        // call to private recursive method.
+        traversePreOrder(root);
+    }
+
+    /**
+     * Recursive method to traverse tree in preOrder traversal(root, left, right).
+     * @param root is the root node of the tree.
+     */
+    private void traversePreOrder(Node root) {
+        // Base case: we check to see if we're at a leaf, hence if we go down a leaf's 'child' we will get a null.
+        if (root == null) {
+            return;
+        }
+        // print the current node's value on the console.
+        System.out.println(root);
+        // this function calls itself on its left child until we get to the base case.
+        traversePreOrder(root.leftChild);
+        // this function calls itself on its right child until we get to the base case.
+        traversePreOrder(root.rightChild);
     }
 }
