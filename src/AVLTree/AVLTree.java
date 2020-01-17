e7263b25c11b3a0c6f52d30dfeb55212ba7c090e
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-01-17 17:04:26 -0500

    Implemented a recursive insert() for AVL Tree class.

--- src/AVLTree/AVLTree.java
+++ src/AVLTree/AVLTree.java
@@ -2,8 +2,37 @@ package AVLTree;
 
 public class AVLTree {
     private class Node{
-        public Node{
+        private int value;
+        private Node leftChild;
+        private Node rightChild;
 
+        public Node(int value){
+            this.value = value;
         }
+    }
+
+    private Node root;
+
+    public void insert(int value){
+        Node node = new Node(value);
+        insert(root, node);
+    }
+
+    private void insert(Node root, Node node){
+        if(node.value < root.value && root.leftChild == null){
+            root.leftChild = node;
+            return;
+        }
+        else if(node.value > root.value && root.rightChild == null){
+            root.rightChild = node;
+            return;
+        }
+        if(node.value < root.value){
+            insert(root.leftChild, node);
+        }
+        else if(node.value > root.rightChild.value){
+            insert(root.rightChild, node);
+        }
+        return;
     }
 }
