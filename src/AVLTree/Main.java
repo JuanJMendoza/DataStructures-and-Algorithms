c8e25d44ac7d9f783e99ef7ef2481786feca11d2
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-02-10 14:12:34 -0500

    Tested a recursive insert() for AVL Tree class. (still needs work)

---
+++ src/AVLTree/Main.java
@@ -0,0 +1,11 @@
+package AVLTree;
+
+public class Main {
+    public static void main(String[] args){
+        AVLTree avlTree = new AVLTree();
+        avlTree.insert(10);
+        avlTree.insert(30);
+        avlTree.insert(1);
+        avlTree.insert(13);
+    }
+}
