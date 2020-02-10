d94da2731aab3056922bdab2a5f3f17c728feca5
Juan J. Mendoza <Juan.J.Mendoza513@Gmail.com>
2020-02-10 15:36:40 -0500

    Implemented methods to check node is left/right heavy or balanced.

--- src/AVLTree/AVLTree.java
+++ src/AVLTree/AVLTree.java
@@ -37,9 +37,28 @@ public class AVLTree {
         }
         // set the current node's height.
         root.height = Math.max(height(root.leftChild), height(root.rightChild)) + 1;
+
+        //Figuring out if the current node is a balanced subtree.
+        if(isLeftHeavy(root)){
+            System.out.println(root.value + " is left heavy.");
+        }
+        else if(isRightHeavy(root)){
+            System.out.println(root.value + " is right heavy.");
+        }
         return root;
     }
 
+    private boolean isLeftHeavy(Node node){
+        return balanceFactor(node) > 1;
+    }
+
+    private boolean isRightHeavy(Node node){
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(Node node){
+        return (node == null) ? 0 : height(node.leftChild) - height(node.rightChild);
+    }
     private int height(Node node){
         return (node == null) ? -1 : node.height;
     }
